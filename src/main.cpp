1c9f4a71b8c0143bd19a2a7d70df908d7367eb52
Anton Shubin <an.shubin@outlook.com>
2022-02-07 17:28:07 +0700

    added rapidjson dependency

--- src/main.cpp
+++ src/main.cpp
@@ -60,7 +60,24 @@ EMSCRIPTEN_BINDINGS(karafuto) {
 
 #include <glm/glm.hpp>
 
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+
 int main() {
+    const char* json = R"({"project":"rapidjson","stars":10})";
+    rapidjson::Document d;
+    d.Parse(json);
+
+    rapidjson::Value& s = d["stars"];
+    s.SetInt(s.GetInt() + 1);
+
+    rapidjson::StringBuffer buffer;
+    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    d.Accept(writer);
+
+    std::cout << buffer.GetString() << std::endl;
+
     const uint16_t viewportWidth{2560};
     const uint16_t viewportHeight{1259};
 
