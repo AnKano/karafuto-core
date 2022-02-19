8f5f8fe965f540fb6c5c224d2c76cb673ccd0409
Anton Shubin <an.shubin@outlook.com>
2022-02-19 11:37:19 +0700

    added resource logic

--- src/core/worlds/BaseWorld.hpp
+++ src/core/worlds/BaseWorld.hpp
@@ -13,6 +13,10 @@
 #include "stages/Stage.hpp"
 #include "../sources/RemoteSource.hpp"
 #include "../misc/STBImageUtils.hpp"
+#include "../sources/local/geojson/GeoJSONObject.hpp"
+#include "../sources/local/geojson/primitives/GeoJSONTransObject.hpp"
+#include "../meshes/PolygonMesh.hpp"
+#include "../meshes/PolylineMesh.hpp"
 
 namespace KCore {
     class BaseWorld {
@@ -237,15 +241,89 @@ namespace KCore {
                                     tile->commitTag("image");
                                 }
                         );
-                        mCreatedBaseTiles[quadcode]->registerDeferResource(
-                                "echo",
+                        mCreatedBaseTiles[quadcode]->registerImmediateResource(
+                                "json",
                                 [](BaseWorld *world, GenericTile *tile) {
+                                    auto desc = tile->getTileDescription();
+                                    auto task = new KCore::CallbackTask{
+                                            [world, desc]() {
+                                                auto tilecode = desc.getTilecode();
+                                                auto zoom = tilecode.z, x = tilecode.x, y = tilecode.y;
+                                                auto *result = (std::vector<KCore::GeoJSONObject> *) world->getSources()["json"]->getDataForTile(
+                                                        zoom, x, y);
+
+                                                auto size = result->size();
+                                                auto *objects = new std::vector<KCore::GeoJSONTransObject>();
+
+                                                for (int i = 0; i < size; i++) {
+                                                    auto &ref = (*result)[i];
+                                                    KCore::GeoJSONTransObject obj{
+                                                            ref.mType,
+                                                            (int) (ref.mMainShapeCoords.size()),
+                                                            (int) (ref.mHoleShapeCoords.size()),
+                                                            nullptr, nullptr
+                                                    };
+
+                                                    auto convertedMain = std::vector<std::array<double, 2>>{};
+                                                    auto convertedHole = std::vector<std::array<double, 2>>{};
+
+                                                    if (obj.mainShapeCoordsCount > 0) {
+                                                        obj.mainShapePositions = new glm::vec3[obj.mainShapeCoordsCount];
+
+                                                        for (int idx = 0; idx < obj.mainShapeCoordsCount; idx++) {
+                                                            auto project = world->latLonToWorldPosition(
+                                                                    {ref.mMainShapeCoords[idx][1],
+                                                                     ref.mMainShapeCoords[idx][0]
+                                                                    });
+                                                            convertedMain.push_back({project.x, project.y});
+                                                            obj.mainShapePositions[idx] = {project.x, 0.0f, project.y};
+                                                        }
+                                                    }
+
+                                                    if (obj.holeShapeCoordsCount > 0) {
+                                                        auto coordsCount = obj.holeShapeCoordsCount;
+                                                        obj.holeShapePositions = new glm::vec3[coordsCount];
+
+                                                        for (int idx = 0; idx < coordsCount; idx++) {
+                                                            auto project = world->latLonToWorldPosition(
+                                                                    {ref.mHoleShapeCoords[idx][1],
+                                                                     ref.mHoleShapeCoords[idx][0]
+                                                                    });
+                                                            convertedHole.push_back({project.x, project.y});
+                                                            obj.holeShapePositions[idx] = {project.x, 0.0f, project.y};
+                                                        }
+                                                    }
+
+                                                    if (ref.mType == KCore::Polyline)
+                                                        obj.mesh = new KCore::PolylineMesh(ref, convertedMain);
+
+
+                                                    if (ref.mType == KCore::Polygon ||
+                                                        ref.mType == KCore::PolygonWithHole)
+                                                        obj.mesh = new KCore::PolygonMesh(ref, convertedMain,
+                                                                                          convertedHole);
+
+                                                    objects->push_back(obj);
+                                                }
+
+                                                delete result;
+
+                                                if (objects->empty()) return;
+
+                                                auto event = KCore::MapEvent::MakeGeoJSONEvent(desc.getQuadcode(),
+                                                                                               objects);
+                                                world->pushToAsyncEvents(event);
+                                            }
+                                    };
+
+                                    world->getTaskContext().pushTaskToQueue(task);
+
+
                                     std::cout
-                                            << tile->getTileDescription().getQuadcode()
-                                            << " on final"
+                                            << world->mIteration
+                                            << " json downloading succesfull!"
                                             << std::endl;
-                                },
-                                {"image"});
+                                });
 
                         mCreatedBaseTiles[quadcode]->invokeResources();
                     }
